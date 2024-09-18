c6bfbafcb3435a577944187367558b5aa035fd77
Arthur Lopes <arthurlopr12@gmail.com>
2024-09-18 19:55:14 -0300

    first commit

---
+++ src/main/java/com/codeinbook/CodeInBookApplication.java
@@ -0,0 +1,13 @@
+package com.codeinbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeInBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeInBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeinbook/CodeInBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeinbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeInBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
