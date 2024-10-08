425b29b585cfe4e2aaf25b707d6f38050ae200b7
Arthur Lopes <arthurlopr12@gmail.com>
2024-10-08 18:57:21 -0300

    fix: code smell

--- src/main/java/com/codeinbook/application/adapter/in/controller/BookController.java
+++ src/main/java/com/codeinbook/application/adapter/in/controller/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/books")
 public class BookController {
 
-    private BookServicePort bookServicePort;
+    private final BookServicePort bookServicePort;
 
     public BookController(BookServicePort bookServicePort) {
         this.bookServicePort = bookServicePort;
