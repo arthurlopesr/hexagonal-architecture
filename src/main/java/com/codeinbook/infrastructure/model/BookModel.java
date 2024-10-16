dd2df3f47a2befef51d3e79aef6f60b3bd887422
Arthur Lopes <arthurlopr12@gmail.com>
2024-10-16 19:49:35 -0300

    feat: add conection with mongo db

--- src/main/java/com/codeinbook/infrastructure/model/BookModel.java
+++ src/main/java/com/codeinbook/infrastructure/model/BookModel.java
@@ -11,23 +11,48 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 @Document
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class BookModel {
+
+    public BookModel(String bookId, String title, String subtitle, String description, BigDecimal price) {
+        this.bookId = bookId;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.description = description;
+        this.price = price;
+    }
 
     @Id
     private String bookId;
 
-    private String title;
+    private final String title;
 
-    private String subtitle;
+    private final String subtitle;
 
-    private String description;
+    private final String description;
 
-    private BigDecimal price;
+    private final BigDecimal price;
 
     public void with(BookDTO bookDTO) {
         this.bookId = Objects.nonNull(bookDTO.bookId()) ? bookDTO.bookId() : this.bookId;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
     }
 }
